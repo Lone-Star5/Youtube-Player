915c6e4e31f7dfa983ddedb684159ec53ce8b3ca
Aman Kumar Singh <amankumar09jul@gmail.com>
2020-06-24 14:21:21 +0530

    Search form and play is working

--- download.py
+++ download.py
@@ -2,6 +2,6 @@ import pytube
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
-link = "https://www.youtube.com/watch?v=vRt7JdyFBFc"
+link = url
 yt = pytube.YouTube(link)
 stream = yt.streams.first()
